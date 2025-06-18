f6015d2f1d3730460de7d84d665a135c238e5b63
laydab <aydenbadyal@gmail.com>
2025-06-17 21:12:19 -0700

    Add VANTA.WAVES background effect with customizable properties

---
+++ script.js
@@ -0,0 +1,10 @@
+VANTA.WAVES({
+    el: "#vanta-bg",
+    mouseControls: true,
+    scale: 1.00,
+    color: 0xCACAD0,
+    shininess: 75.00,
+    waveHeight: 10.0,
+    waveSpeed: 0.50,
+    zoom: 0.80
+  });
